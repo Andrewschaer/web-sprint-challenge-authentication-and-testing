d0510ad7af006256c1d09067e9742b348a4d7682
Andrewschaer <andrewschaer@github.com>
2021-09-17 17:29:16 -0400

    added auth login endpoint w middlware and tests - tests passing

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,11 +1,11 @@
 const router = require('express').Router();
-const { checkUsernameExists, checkUsernameFree, validateNewUser } = require('./auth-middleware')
+const { checkUsernameExists, checkUsernameFree, validateUser } = require('./auth-middleware')
 const tokenBuilder = require('./token-builder');
 const bcrypt = require('bcryptjs');
 const User = require('../users/users-model');
 
 
-router.post('/register', validateNewUser, checkUsernameFree, (req, res, next) => {
+router.post('/register', validateUser, checkUsernameFree, (req, res, next) => {
   let user = req.body;
   const rounds = process.env.BCRYPT_ROUNDS || 8; // 2 ^ 8
   const hash = bcrypt.hashSync(user.password, rounds);
@@ -18,31 +18,19 @@ router.post('/register', validateNewUser, checkUsernameFree, (req, res, next) =>
     .catch(next);
 });
 
-router.post('/login', (req, res) => {
-  res.end('implement login, please!');
-  /*
-    IMPLEMENT
-    You are welcome to build additional middlewares to help with the endpoint's functionality.
-
-    1- In order to log into an existing account the client must provide `username` and `password`:
-      {
-        "username": "Captain Marvel",
-        "password": "foobar"
-      }
-
-    2- On SUCCESSFUL login,
-      the response body should have `message` and `token`:
-      {
-        "message": "welcome, Captain Marvel",
-        "token": "eyJhbGciOiJIUzI ... ETC ... vUPjZYDSa46Nwz8"
-      }
-
-    3- On FAILED login due to `username` or `password` missing from the request body,
-      the response body should include a string exactly as follows: "username and password required".
-
-    4- On FAILED login due to `username` not existing in the db, or `password` being incorrect,
-      the response body should include a string exactly as follows: "invalid credentials".
-  */
+router.post('/login', validateUser, checkUsernameExists, (req, res, next) => {
+  let { password } = req.body;
+  const user = req.body.foundUser
+  
+  if (bcrypt.compareSync(password, user.password)) {
+    const token = tokenBuilder(user)
+    res.status(200).json({
+      message: `welcome, ${user.username}`,
+      token,
+    });
+  } else {
+    next({ status: 401, message: 'invalid credentials' });
+  }
 });
 
 module.exports = router;
